7ef526b079f4db8650829f5c85e89cdb4a9658fb
Fabricio <fabricio@Fabricios-MBP.lan>
2023-01-14 12:59:32 +0000

    init

---
+++ src/main/java/com/github/fabriciossouza/rickandmortyapi/RickandmortyapiApplication.java
@@ -0,0 +1,13 @@
+package com.github.fabriciossouza.rickandmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickandmortyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickandmortyapiApplication.class, args);
+	}
+
+}
